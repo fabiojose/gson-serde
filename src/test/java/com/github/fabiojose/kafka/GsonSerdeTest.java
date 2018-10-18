636aa03717a48543fac7855ffe7354a384ae6b2b
Fabio José <fabiojose@gmail.com>
2018-10-17 21:31:55 -0300

    Test cases

---
+++ src/test/java/com/github/fabiojose/kafka/GsonSerdeTest.java
@@ -0,0 +1,50 @@
+package com.github.fabiojose.kafka;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class GsonSerdeTest extends TestCase {
+	
+	private static final Gson GSON = new Gson();
+	
+	private static final String JSON = "{\"name\":\"fabiojose\"}";
+	
+	private static final byte[] BYTES = JSON.getBytes(Charset.forName("UTF-8"));
+	
+	private static final String TOPIC = "my.topic";
+	
+	private static JsonObject TO_TEST = GSON.fromJson(JSON, JsonObject.class);
+
+	public GsonSerdeTest( String testName ) {
+        super( testName );
+    }
+	
+	public static Test suite() {
+        return new TestSuite( GsonSerdeTest.class );
+    }
+	
+	public void testDeserializer() {
+		try(GsonDeserializer<JsonObject> de = new GsonDeserializer<>(JsonObject.class)){
+		
+			JsonObject actual = de.deserialize(TOPIC, BYTES);
+			assertEquals(TO_TEST, actual);
+			
+		}
+	}
+	
+	public void testSerializer() {
+		try(GsonSerializer<JsonObject> se = new GsonSerializer<>()){
+			
+			byte[] actual = se.serialize(TOPIC, TO_TEST);
+			assertTrue("Teste do serializer falhou", Arrays.equals(BYTES, actual));
+			
+		}
+	}
+}
